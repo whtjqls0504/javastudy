3bfee4c92b0b28a32fee75710a491ad68fd56a09
whtjqls0504 <whtjqls0504@naver.com>
2023-07-27 17:47:59 +0900

    230727\

---
+++ 12_CollectionFramework_Ex/src/ex03_Bus/Person.java
@@ -0,0 +1,32 @@
+package ex03_Bus;
+
+public class Person {
+
+  private String name;
+  
+  public Person() {
+    // TODO Auto-generated constructor stub
+  }
+
+  public Person(String name) {
+    super();
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Person [name=" + name + "]";
+  }
+
+  
+  
+
+}
---
+++ 12_CollectionFramework_Ex/src/ex03_Bus/Student.java
@@ -0,0 +1,14 @@
+package ex03_Bus;
+
+public class Student extends Person {
+
+  public Student(String name) {
+    super(name);
+    // TODO Auto-generated constructor stub
+  }
+  
+  // 필드가 없는데 생성자가 필요할때는 상속관계일때. 
+  // new Student("홍길동")
+
+  
+}
