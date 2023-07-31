1747dd780d75750df3921d7c30e7117a7f7ac1e3
whtjqls0504 <whtjqls0504@naver.com>
2023-07-31 17:00:22 +0900

    230731

--- 12_CollectionFramework_Ex/src/ex09_DayOfTheWeek/MainWrapper.java
+++ 12_CollectionFramework_Ex/src/ex09_DayOfTheWeek/MainWrapper.java
@@ -4,7 +4,7 @@ public class MainWrapper {
 
   public static void main(String[] args) {
     
-    DayOfTheWeek dayOfTheWeek = new DayOfTheWeek(2023, 7, 28);
+    DayOfTheWeek dayOfTheWeek = new DayOfTheWeek(2023, 2, 29);
     System.out.println(dayOfTheWeek);  // 2023년 07월 28일은 금요일입니다.
 
   }
