b705b34b1c8c6ed99eb065a8fb7f82fe6fc4f15e
whtjqls0504 <whtjqls0504@naver.com>
2023-08-01 12:43:12 +0900

    230801

--- 15_Exception/src/ex01_try_catch/MainWrapper.java
+++ 15_Exception/src/ex01_try_catch/MainWrapper.java
@@ -29,6 +29,7 @@ public class MainWrapper {
       int number1 = Integer.parseInt(numbers[0]);   // number form에 예외가 발생할 수 있다.
       int number2 = Integer.parseInt(numbers[1]);
       System.out.println(number1 + number2);
+      System.out.println(number1 - number2);
       sc.close();
     } catch(NumberFormatException e) {
       System.out.println("정수 연산만 가능합니다.");
@@ -55,7 +56,7 @@ public class MainWrapper {
   }
 
   public static void main(String[] args) {
-      ex03();
+      ex02();
   }
 
 }
---
+++ 16_Exception_Ex/src/ex01_BankAccount/MainWrapper.java
@@ -0,0 +1,11 @@
+package ex01_BankAccount;
+
+public class MainWrapper {
+
+  public static void main(String[] args) {
+    
+    BankAccount acc = new BankAccount(10000, null)
+    acc.deposit(-1);
+  }
+
+}
