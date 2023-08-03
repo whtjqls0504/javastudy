696b759b2b40d966fda54ff640bfdea7b96659fc
whtjqls0504 <whtjqls0504@naver.com>
2023-08-03 10:36:46 +0900

    230803

--- 17_Input_Output/src/ex02_OutputStream/MainWrapper.java
+++ 17_Input_Output/src/ex02_OutputStream/MainWrapper.java
@@ -1,8 +1,10 @@
 package ex02_OutputStream;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class MainWrapper {
 
@@ -76,7 +78,7 @@ public class MainWrapper {
       fout = new FileOutputStream(file);
       
       String s = "안녕하세요";
-      byte[] b = s.getBytes();
+      byte[] b = s.getBytes("UTF-8");
 
       fout.write(b);
        
@@ -95,8 +97,58 @@ public class MainWrapper {
     System.out.println(file.getPath() + " 파일 크기: " + file.length() + "바이트");
   }
   
+  public static void ex03() {
+    
+    
+    // 2줄로 안녕하세요 \n 반갑습니다.  
+    File dir = new File("C:/storage");
+    if(!dir.exists()) {
+      dir.mkdirs();
+    } 
+    
+    File file = new File(dir, "ex03.dat");
+    
+    // 버퍼출력 스트림 선언
+    BufferedOutputStream bout = null;
+    
+    try {
+      // 버퍼출력스트림 생성(반드시 예외처리 필요한 코드이다.)
+      bout = new BufferedOutputStream(new FileOutputStream(file));
+    
+      String s1 ="안녕하세요";
+      String s2 ="반갑습니다";
+      int c = '\n';
+
+      bout.write(s1.getBytes("UTF-8"));
+      bout.write(c);
+      bout.write(s2.getBytes(StandardCharsets.UTF_8));  // getByte("UTF-8")과 동일하다
+      
+      
+    } catch (IOException e) {
+          e.printStackTrace();
+    } finally {
+      try {
+        if (bout!=null) {
+          bout.close();
+        }
+      } catch (IOException  e) {
+        e.printStackTrace();
+      }
+    }
+        
+    System.out.println(file.getPath() + " 파일 크기 : " + file.length());
+    
+  }
+
+  /*
+   *   java.io.BufferedOutputStream 클래스
+   *  1. 내부 버퍼를 가지고 있는 출력스트림이다.
+   *  2. 많은 데이터를 한 번에 출력하기 때문에 속도 향상을 위해서 사용한다.
+   *  3. 보조스트림이므로 메인스트림과 함께 사용한다. (fileoutputStream)
+   */
+  
   public static void main(String[] args) {
-    ex02();
+    ex03();
   }
 
 }
