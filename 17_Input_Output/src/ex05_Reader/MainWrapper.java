d97da546d11058e5e91f2f02fc3adcc97c654784
whtjqls0504 <whtjqls0504@naver.com>
2023-08-04 12:44:00 +0900

    230804

--- 17_Input_Output/src/ex05_Reader/MainWrapper.java
+++ 17_Input_Output/src/ex05_Reader/MainWrapper.java
@@ -1,8 +1,13 @@
 package ex05_Reader;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class MainWrapper {
 
@@ -87,24 +92,23 @@ public class MainWrapper {
     try {
       
       fr = new FileReader(file);
-      
+
       // 입력된 문자 저장 배열
       char[] cbuf = new char[2];
-      
+
       // 실제로 읽은 글자 수 저장 변수
       int readChar = 0;
-      
+
       // 입력된 문자를 누적할 StringBuffer 생성
       StringBuffer sb = new StringBuffer();
-      
+
       // read(char[] cbuf) 메소드
       // 1. 파라미터로 전달 된 char[] cbuf에 읽은 문자를 저장한다.
       // 2. 실제로 읽은 글자 수를 반환한다.
       // 3. 읽은 문자가 없으면 -1을 반환한다.
-      
 
       // 반복문 : 읽은 문자가 -1 아니면 계속 읽는다.
-      while((readChar = fr.read(cbuf)) != -1) {
+      while ((readChar = fr.read(cbuf)) != -1) {
         sb.append(cbuf, 0, readChar); // 배열 cbuf의 인덱스 0부터 readChar개 문자를 sb에 추가한다.
       }
       
@@ -138,32 +142,30 @@ public class MainWrapper {
     File file = new File(dir, "ex03.txt");
 
     
-    // 파일입력스트림 선언
-    FileReader fr = null;
+    // 버퍼입력스트림 선언
+    BufferedReader br = null;
     
     try {
       
-      fr = new FileReader(file);
+      // 버퍼입력스트림 생성(반드시 예외처리 필요, 파일이 없으면 FileNotFoundException)
+      br = new BufferedReader(new FileReader(file));
       
-      // 입력된 문자 저장 배열
-      char[] cbuf = new char[2];
-      
-      // 실제로 읽은 글자 수 저장 변수
-      int readChar = 0;
+      // 입력된 문자열 저장 변수
+      String line = null;
       
       // 입력된 문자를 누적할 StringBuffer 생성
       StringBuffer sb = new StringBuffer();
       
-      // read(char[] cbuf) 메소드
-      // 1. 파라미터로 전달 된 char[] cbuf에 읽은 문자를 저장한다.
-      // 2. 실제로 읽은 글자 수를 반환한다.
-      // 3. 읽은 문자가 없으면 -1을 반환한다.
+      // String readLine() 메소드
+      // 1. 한 줄 전체를 반환한다.
+      // 3. 읽은 문자가 없으면 Null을 반환한다.
       
 
-      // 반복문 : 읽은 문자가 -1 아니면 계속 읽는다.
-      while((readChar = fr.read(cbuf)) != -1) {
-        sb.append(cbuf, 0, readChar); // 배열 cbuf의 인덱스 0부터 readChar개 문자를 sb에 추가한다.
+      // 반복문 : readLine()의 반환값이 null이 아니면 계속 읽는다.
+      while((line= br.readLine()) != null) {
+        sb.append(line + "\n");  // 읽은 라인에 줄 바꿈(\n)
       }
+      
       
       // 결과 확인
       System.out.println(sb.toString());
@@ -173,8 +175,8 @@ public class MainWrapper {
       e.printStackTrace();
   } finally {
     try {
-      if(fr !=null) {
-        fr.close();
+      if(br !=null) {
+        br.close();
       }
       
     }catch (IOException e) {
@@ -182,16 +184,68 @@ public class MainWrapper {
     }
    
   }
-    
-     
-  
-    
-    
+
   }
+
+  public static void ex04() {
+    
+    // 바이트 출력스트림으로 보낸 문자를 바이트 입력스트림으로 읽기 (한글 실패)
+    // 바이트 출력스트림으로 보낸 문자를 문자 입력스트림으로 읽기
+    
+    // 1단계. 바이트 출력 스트림으로 문자 보내기
+    File dir = new File("C:/storage");
+    
+    if(dir.exists() == false) {
+      dir.mkdirs();
+    }
+
+    File file = new File(dir, "server.dat");
+    BufferedOutputStream bout = null;
+    
+    try {
+      
+      bout = new BufferedOutputStream(new FileOutputStream(file));
+      String s1 = "안녕하세요";
+      String s2 = "Hello";
+      
+      bout.write(s1.getBytes("UTF-8"));
+      bout.write(s2.getBytes("UTF-8"));
+      bout.close();
+    
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    // 2단계. 문자 입력스트림으로 읽기
+    /*
+     * java.io.InputStreamReader 클래스
+     * 1. Reader 클래스를 상속 받는 클래스이다. (문자 입력스트림이다.)
+     * 2. InputStream(바이트 입력스트림)을 받아서 Reader(문자 입력스트림)으로 변환한다.
+     * 
+     */
+    BufferedReader br = null;
+    try {
+      
+      br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+      
+      String line = null;
+      StringBuffer sb = new StringBuffer();
+      
+      while((line = br.readLine()) != null) {
+        sb.append(line);
+      }
+      System.out.println(sb.toString());
+      br.close();
+      
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    
+  }  
   
   
   public static void main(String[] args) {
-    ex02();
+    ex04();
 
   }
 
