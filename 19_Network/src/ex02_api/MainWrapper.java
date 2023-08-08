f0c94ff091b167adf148491bec7f1ba06296ead3
whtjqls0504 <whtjqls0504@naver.com>
2023-08-08 10:09:07 +0900

    230808

--- 19_Network/src/ex02_api/MainWrapper.java
+++ 19_Network/src/ex02_api/MainWrapper.java
@@ -105,9 +105,77 @@ public class MainWrapper {
     }
     
   }
+
+  public static void ex02() {
+    /*
+     * 네이버 개발자센터 - 검색(블로그)
+     * 1. 요청 주소 https://openapi.naver.com/v1/search/blog.json
+     * 2. 요청 변수 (Request Parmeter
+     *      1) query   : 필수, 인코딩된 검색어
+     *      2) display : 선택, 10 (검색 결과의 개수)
+     *      3) start   : 선택, 1  
+     *      4) sort    : 선택, sim
+     * 
+     */
+    
+     URL url = null;
+     HttpURLConnection  con = null;
+     BufferedReader reader = null;
+     
+     try {
+       
+       String spec = "https://openapi.naver.com/v1/search/blog.json";
+       String query = "폭염";
+       String display = "10";
+       String start = "1";
+       String sort = "sim";
+       String clientId = "dxLQ_GbhqsM3QHNFLIB3";
+       String clientSecret = "CsMJ8FIn4F";
+       
+       StringBuilder sbUrl = new StringBuilder();
+       sbUrl.append(spec);
+       sbUrl.append("?query=").append(URLEncoder.encode(query, "UTF-8"));
+       sbUrl.append("&display=").append(display); // 인코딩 한다고 하여 달라지는건 없다.
+       sbUrl.append("&start=").append(start); 
+       sbUrl.append("&sort=").append(sort); 
+     
+       url = new URL(sbUrl.toString());
+       con = (HttpURLConnection)url.openConnection();
+     
+       // 요청 메소드
+       con.setRequestMethod("GET");   // 반드시 대문자 GET
+       
+       // 요청 헤더
+       con.setRequestProperty("X-Naver-Client-Id", clientId);
+       con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+       
+       int responseCode = con.getResponseCode();
+       if(responseCode != HttpURLConnection.HTTP_OK) {
+         throw new RuntimeException(responseCode + " 발생");
+       }
+       
+       reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+       StringBuilder sb = new StringBuilder();
+       String line = null;
+       while((line = reader.readLine()) != null) {
+         sb.append(line);
+         
+       }
+       System.out.println(sb.toString());
+     } catch (Exception e) { 
+       System.out.println(e.getMessage());
+    } finally {
+      try {
+        if(reader != null) reader.close();
+        if(con != null) con.disconnect();
+      }catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
   
   public static void main(String[] args) {
-    ex01();
+    ex02();
   }
 
 }
